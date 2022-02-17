818a7ca4fb996d348c1eaef0e46973e1cafc0d30
qkrwn <qkrwnstj@gmail.com>
2022-02-16 22:04:31 -0800

    refactored

--- python/test/priorityQueue.py
+++ python/test/priorityQueue.py
@@ -1,59 +1,66 @@
-# Author: Joonseo Park
-
 class SimplePriorityQueue( object ):
     '''
         Simple priority queue class
         stream out only priority items found in user defined priority keys
     '''
-    PRIORITY_KEYS = [0, 10]
+    PRIORITY_KEY = [0, 10]
     def __init__(self):
-        self._queue = []
-
+        '''
+        construtor
+        '''
+        self.queue = []
+  
     def __repr__(self):
         return '( "%s" )' % ( self.__class__.__name__ )
 
     def __str__(self):
-        return " ".join(["%s"%stream for stream in self.queue])
-    
-    @property
-    def queue(self):
-        return self._queue
-
+        return ' '.join(["%s"%i for i in self.queue])
+  
     @property
     def size(self):
         return len(self.queue)
 
     def is_empty(self):
-        return self.size == 0
+        return (self.size == 0)
 
+    # add queue
     def add(self, data):
         self.queue.append(data)
-
-    # delete queues if its key is not in PRIORITY_KEYS
-    def set(self):
+  
+    # return removed item based on the key's priority
+    def remove(self):
         try:
-            i = 0
-            for i in range(len(self._queue)):
-                if not self.queue[i].keys()[0] in self.PRIORITY_KEYS:
-                    del self._queue[i]
-        except IndexError:
-            pass
+            item = None
+            idx = 0
+            for i in range(len(self.queue)):
+                key = list(self.queue[i].keys())[0]
+                if key in self.PRIORITY_KEY:
+                    idx = i
+                    break
+            item = self.queue[idx]
+            del self.queue[idx]
+            return item
+        except IndexError as e:
+            print(e)
+            exit()
 
 
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     streams = [
-        {0: "Jenny"},
-        {20: "Tom"},
-        {10: "John"},
-        {5: "Brad"},
-        {0: "Joonseo"},
-        {13: "Tommny"}
+        {0: "Jenny_cmd"},
+        {20: "Tom_cmd"},
+        {10: "John_cmd"},
+        {5: "Brad_cmd"},
+        {0: "Joonseo_cmd"},
+        {13: "Tommny_cmd"}
     ]
     myQueue = SimplePriorityQueue()
     for stream in streams:
         myQueue.add(stream)
-    print(myQueue)
-    myQueue.set()
-
-    print(myQueue)
+    # dequeue in the priority order
+    results = []
+    while not myQueue.is_empty():
+        results.append(myQueue.remove())
+    print(results)
